23653323182bbb6eed18ebca2e6cd7b92831e14c
codsim <129361543+uvcodin@users.noreply.github.com>
2023-07-14 13:33:52 +0530

    Create HomeworkNo1.py

---
+++ HomeworkNo1.py
@@ -0,0 +1,41 @@
+// Assignment Operator
+
+x = 3
+y = 2
+
+console.log(x)
+console.log(y)
+
+//Comparision Operator
+
+console.log(x==y)
+console.log(x!=y)
+console.log(x<y)
+console.log(x>y)
+
+//Arithmetic Operator
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+
+//Conditional Operator
+
+x > y ? console.log("ok") : console.log("not ok")
+
+//Logical Operator
+
+console.log(x==3 || y==4)
+console.log(x==3 && y==2)
+
+//Bitwise Operator
+
+console.log(x|y)
+console.log(x&y)
+console.log(x^y)
+console.log(x<<y)
+console.log(x>>y)
+console.log(~y)
+console.log(x>>>y)
