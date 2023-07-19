095aad3ce8fc26e5e70b366db9737349c24d2924
codsim <129361543+uvcodin@users.noreply.github.com>
2023-07-19 14:23:17 +0530

    Create TypeConsversion.py

---
+++ TypeConsversion.py
@@ -0,0 +1,17 @@
+// Type Conversion
+
+let x;
+x = String(27);
+console.log(x, typeof (x));
+
+let boolx = String(true);
+console.log(boolx, typeof (boolx));
+
+let arrx = String([1,4,6]);
+console.log(arrx, typeof (arrx));
+
+
+
+
+
+
