41b40fdece9893aacd8d6ea90c9850f3cd2bde7f
codsim <129361543+uvcodin@users.noreply.github.com>
2023-07-19 14:31:17 +0530

    Create Typeconversion.py

---
+++ Typeconversion.py
@@ -0,0 +1,34 @@
+// Type Conversion
+
+let x;
+x = String(27);
+console.log(x, typeof (x));
+
+let boolx = String(true);
+console.log(boolx, typeof (boolx));
+
+let arrx = String([1,4,6]);
+console.log(arrx, typeof (arrx));
+
+let abc = Number("2725");
+console.log(abc, typeof (abc));
+
+abc = Number("272m5");
+console.log(abc, typeof (abc));
+
+
+abc = Number(false);
+console.log(abc, typeof (abc));
+
+abc = Number([0,6,25,27,29]);
+console.log(abc, typeof (abc));
+
+let num = parseInt('27')
+console.log(num, typeof (num));
+
+
+
+
+
+
+
